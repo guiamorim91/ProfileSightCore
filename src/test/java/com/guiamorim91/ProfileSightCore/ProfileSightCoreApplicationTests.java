8c42c03fc9121d293526bd2ebf62b3f96b4dabe1
Guilherme Amorim <guiamorim91@gmail.com>
2023-10-20 16:56:34 -0300

    First commit

---
+++ src/test/java/com/guiamorim91/ProfileSightCore/ProfileSightCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiamorim91.ProfileSightCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileSightCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
